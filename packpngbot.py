23aca1356b01a760168766a63c1c1f53649dd183
Cactus <cactusduper1@gmail.com>
2020-01-26 13:43:58 -0600

    comments and stuff for people who dont know code

--- packpngbot.py
+++ packpngbot.py
@@ -12,8 +12,10 @@ bot = commands.Bot(command_prefix=commands.when_mentioned_or('^'), description='
 bot.remove_command("help")
 @bot.event
 async def on_message(message):
-    if message.author.id != 667447141068832768:
-        if message.channel.id == 666575359411748875 or message.channel.id == 666758275504537604 or message.channel.id == 666813360867770388 or message.channel.id == 660701994549379125:
+    if message.author.id != 667447141068832768: # prevents bot from going in a loop and responding to itself 
+        if message.channel.id == 666575359411748875 or message.channel.id == 666758275504537604 or message.channel.id == 666813360867770388 or message.channel.id == 660701994549379125: # channel ids
+            # if 'word' in message.content.lower():  checks for word in message
+            #     await message.channel.send("Hey, it looks like you mentioned word")  responds with this if its found
             if 'dream' in message.content.lower():
                 await message.channel.send("Hey, it looks like you mentioned Dream! Unfortunately we cannot use that method because we don't have enough information. But here's some other ideas: {link to a google doc}")
             if 'have you tried' in message.content.lower():
@@ -31,19 +33,20 @@ async def on_message(message):
             if 'quantum computer' in message.content.lower():
                 await message.channel.send("Hey, it looks like you mentioned a quantum computer! Unfortunately this won't help with this problem and we already have enough computing power")
             if 'readthepins' in message.content.lower():
-                file1 = open("counter.txt", "r+")
-                counter = file1.readline()
-                counter = int(counter)
-                counter = counter+1
-                counter = str(counter)
-                file1.seek(0)
-                file1.truncate()
-                file1.write(counter)
-                await message.channel.send("PUT SOME FAQ STUFF HERE. this faq has been sent: {0} times.".format(counter))
+                file1 = open("counter.txt", "r+") # opens file
+                counter = file1.readline() # reads the line
+                counter = int(counter) # str to int
+                counter = counter+1 # counter + 1
+                counter = str(counter) # int to str
+                file1.seek(0) # goes to start of file
+                file1.truncate() # yeets the line
+                file1.write(counter) # yeets counter back in
+                file1.close() # closes the file
+                await message.channel.send("PUT SOME FAQ STUFF HERE. this faq has been sent: {0} times.".format(counter)) # sends message with counter. hopefully this makes it ez for people who dont know code what this does
 
-@bot.event
-async def on_ready():
-    await bot.change_presence(activity=discord.Game(name='responding to pack.png questions', type=0, url='https://packpng.com'))
-    logging.info('Logged in as:{0} (ID: {0.id})'.format(bot.user))
-bot.run(auth['token'])
+@bot.event 
+async def on_ready(): # after everything is done and stuff get ready to login and respond to gentards
+    await bot.change_presence(activity=discord.Game(name='responding to pack.png questions', type=0, url='https://packpng.com/faq')) # sets description and stuff
+    logging.info('Logged in as:{0} (ID: {0.id})'.format(bot.user)) # to console
+bot.run(auth['token']) # login
 
